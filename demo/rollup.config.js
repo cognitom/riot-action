286ee69e5e34b9b6f73e0542cb3d0098ccbcab61
Tsutomu Kawamura <cognitom@gmail.com>
2016-04-02 00:19:05 +0900

    Uses rollup-plugin-riot

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -1,34 +1,15 @@
-import babel            from 'rollup-plugin-babel'
-import npm              from 'rollup-plugin-npm'
-import commonjs         from 'rollup-plugin-commonjs'
-import { createFilter } from 'rollup-pluginutils'
-import compiler         from 'riot-compiler'
+import riot from 'rollup-plugin-riot'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
 
 export default {
   entry: 'src/main.js',
   dest: 'dist/bundle.js',
   plugins: [
     riot(),
-    npm({
-      jsnext:  true, // if provided in ES6
-      main:    true, // if provided in CommonJS
-      browser: true  // if provided for browsers
-    }),
+    resolve({ jsnext: true, main: true, browser: true }),
     commonjs(),
     babel()
   ]
-}
-
-/**
- * Simple inline-plugin for Riot.js
- */
-function riot() {
-  const frag = "import riot from 'riot';"
-  const filter = createFilter('**/*.tag') // transform tag files only
-  return {
-    transform (code, id) {
-      if (!filter(id)) return null
-      return frag + compiler.compile(code)
-    }
-  }
 }
