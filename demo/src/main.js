59db917c12234f8121f052114e2662743990bc3b
Tsutomu Kawamura <cognitom@gmail.com>
2016-04-02 00:22:49 +0900

    Update demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,9 +1,9 @@
-import riot     from 'riot'
-import route    from 'riot-route'
-import { util } from 'riot-action'
+import riot from 'riot'
+import route from 'riot-route'
+import { view, mixin } from 'riot-action'
 
 // actions
-import Menu    from './action-menu'
+import Menu from './action-menu'
 import Notepad from './action-notepad'
 import History from './action-history'
 
@@ -12,10 +12,13 @@ import './app-menu.tag'
 import './app-notepad.tag'
 import './app-history.tag'
 
+// registers riot-action as a middleware
+riot.mixin(mixin)
+
+// routings
 riot.mount('#menu', 'app-menu', { action: Menu })
-util.mount('#container', [
-  { route: 'notepad/*', tag: 'app-notepad', options: { action: Notepad } },
-  { route: 'history',   tag: 'app-history', options: { action: History } }
-])
+view.mount('#container')
+  .route('notepad/*', 'app-notepad', { action: Notepad })
+  .route('history', 'app-history', { action: History })
 
 route.start(true)
